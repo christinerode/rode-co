27e8678e53cf1f0b34e37b10ffb399219c1d77ae
Christine Røde <hi@christinerode.com>
2019-03-10 13:18:27 +0000

    create-react-app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+  render() {
+    return (
+        <div
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (

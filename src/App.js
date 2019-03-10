016913702bf338cf4c83ed43bbeeec09c576820c
Christine Røde <hi@christinerode.com>
2019-03-10 14:50:59 +0000

    base layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,76 @@ import './App.css';
 class Header extends Component {
   render() {
     return (
-        <div
+        <div className="w-100">
+          <div className="name fl w-70 w-20-ns f6">Christine Røde</div>
+           <ul className="light fl w-30 w-80-ns list ma0 pa0 f6 mb4">
+              <Social url="http://twitter.com/chrstnerode">Twitter</Social>
+              <Social url="http://instagram.com/christinerode">Instagram</Social>
+            </ul>
+        </div>
+    );
+  }
+}
+
+const Social = ({children, url}) => (
+  <li className="m0 p0 tr-s">
+    <a href="${url}">
+      {children}
+    </a>
+  </li>
+)
+
+const Title = ({children}) => (
+  <div className="cf fl w-100 mt5 mt6-ns">
+    <div className="fl w-20">
+      <h2 className="ma0 f7">
+        {children}
+      </h2>
+    </div>
+    <div className="fl w-80">
+      <span className="h2 f7">&nbsp;</span>
+    </div>
+  </div>
+)
+
+const Experience = ({children, range, place, no}) => (
+  <div className="fl w-100 mt5">
+    <div className="fl w-20 fw0 f7 light">
+       {no}
+    </div>
+    <div className="fl w-80 f4 f3-ns">
+      <div className="fl w-100 light lh-title">
+        {range}
+      </div>
+      <h3 className="f3 f2-ns mv2 fl w-100 w-70-ns fw4 lh-title">
+        {children}
+      </h3>
+      <div className="fr-ns w-100 w-20-ns mv2 place tl-ns f2-ns v-btm">
+        <span className="f6 sans-serif place lh-copy">
+          {place}
+        </span>
+      </div>
+    </div>
+  </div>
+)
+
+class Resume extends Component {
+  render() {
+    return (
+        <div className="fl w-100 mb2">
+          <Title>Experience</Title>
+
+          <Experience range="2017–" place="London" no="01">
+              Sr. Product Designer, Deliveroo
+          </Experience>
+          <Experience range="2016–2017" place="Amsterdam / Stockholm" no="02">
+              Lead Designer, Freelance
+          </Experience>
+          <Experience range="2013–2016" place="California / London" no="03">
+              Product Designer, Facebook
+          </Experience>
+          <Experience range="&c" no="-"></Experience>
+        </div>
     );
   }
 }
@@ -13,22 +82,11 @@ class Header extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+       <Header />
+       <Resume />
+       <Title>Bio</Title>
+     </div>
     );
   }
 }

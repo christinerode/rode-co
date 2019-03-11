c23c171abe1c8f3dbb929b554dbf91f36d76ad84
Christine Røde <cineee@gmail.com>
2019-03-11 20:57:58 +0000

    pretentious symbols 2

--- src/App.js
+++ src/App.js
@@ -89,8 +89,8 @@ class Resume extends Component {
 
           <Title>Intro</Title>
 
-          <Paragraph no="§">I'm Christine, a digital product designer passion&shy;ate about people, cultures, and&nbsp;places. </Paragraph>
-          <Paragraph no="¶">With a focus on mobile apps, I'm happest when I'm solving complex problems through elegant interactions and user-friendly inter&shy;faces. </Paragraph>
+          <Paragraph no="¶">I'm Christine, a digital product designer passion&shy;ate about people, cultures, and&nbsp;places. </Paragraph>
+          <Paragraph no="-">With a focus on mobile apps, I'm happest when I'm solving complex problems through elegant interactions and user-friendly inter&shy;faces. </Paragraph>
 
           <div className="cf pa5"></div>
 
